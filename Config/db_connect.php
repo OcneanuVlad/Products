24a7aa7683e4ec7b32938f76d1ece5d6cdc3af77
OcneanuVlad <ocniocni@yahoo.ro>
2023-03-25 13:30:59 +0200

    Updated information to match the new database

--- Config/db_connect.php
+++ Config/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 // connect to database
-$conn = mysqli_connect('localhost', 'vlad', 'test', 'products');
+$conn = mysqli_connect('localhost', 'vlad', 'XEa5xF<W)8uLmX{4', 'id20483326_products');
 
 // check connection
 if(!$conn) {
